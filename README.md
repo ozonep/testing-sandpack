7f75286fa5ff16d948b7bb4f2cea6a4153633cfe
ozonep <ozonep@yandex.ru>
2019-06-23 22:39:10 +0200

    updated everything

--- README.md
+++ README.md
@@ -5,17 +5,13 @@ Exploring codesandbox api capabilities
 ## Install
 
 ```sh
-$ yarn install
+$ npm install
 ```
 
 ## Usage
 
 ```sh
-$ yarn start # localhost:3000
+$ npm run start 
+
+# localhost:3000
 ```
-
-## Links
-
-- https://github.com/CompuIves/codesandbox-client/tree/3cb399a3b2b4a43c5b37fab453e177199b34e191/packages/sandpack
-- https://github.com/CompuIves/codesandbox-client/blob/3cb399a3b2b4a43c5b37fab453e177199b34e191/packages/react-sandpack/README.md
-- fhttps://github.com/brumm/atom-codesandbox
--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import 'react-smooshpack/dist/styles.css';
+import { Manager } from 'smooshpack';
+import React from "react";
 
-import {
-  FileExplorer,
-  CodeMirror,
-  BrowserPreview,
-  SandpackProvider,
-} from 'react-smooshpack';
 
-const files = {
-  '/index.js': {
-    code: "document.body.innerHTML = `<div>${require('uuid/v4')()}</div>`",
-  },
+const manager = new Manager(
+    '#preview',
+    {
+      files: {
+        '/index.js': {
+          code: `console.log('lol it works')`,
+        },
+        '/index.html': {
+          code: `<link rel="stylesheet" type="text/css" href="./public/style.css"><p>HAAA<a href="./index2.html">I am cool</a></p><script src="./index.js"></script>`
+        },
+        '/index2.html': {
+          code: `<style rel="stylesheet" href="./public/style.css"></style><p>I am cool2</p>`
+        },
+        '/public/style.css': {
+          code: `p {color: blue}`
+        }
+      },
+      entry: '/index.html',
+      dependencies: {
+        uuid: 'latest',
+        "@babel/core": "7.2.0",
+        "parcel-bundler": "^1.6.1"
+      },
+    }
+);
+// When you make a change you can just run `updatePreview`, we'll automatically discover
+// which files have changed and hot reload them.
+let edit = () => {
+  manager.updatePreview({
+    files: {
+      '/index.js': {
+        code: `console.log('New Text!')`,
+      },
+      '/index.html': {
+        code: `<p>I am cool</p><script src="/index.js"></script>`
+      }
+    },
+    entry: '/index.html',
+    dependencies: {
+      uuid: 'latest',
+    },
+  });
 };
 
-const dependencies = {
-  uuid: 'latest',
-};
+
 
 const App = () => (
-  <SandpackProvider files={files} dependencies={dependencies} entry="/index.js">
-    <div style={{ display: 'flex', width: '100%', height: '100%' }}>
-      <FileExplorer style={{ width: 300 }} />
-      <CodeMirror style={{ flex: 1 }} />
-      <BrowserPreview style={{ flex: 1 }} />
-    </div>
-  </SandpackProvider>
+    <div id="o"></div>
 );
-
 
 export default App;
